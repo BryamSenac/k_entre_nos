601fac841b92c06e590c2150df0684e2ed5c55e0
aliciaab <alicia.5959.senac@gmail.com>
2024-10-24 14:34:27 -0300

    f

--- controller/index.js
+++ controller/index.js
@@ -5,9 +5,3 @@ window.puxarLogin = puxarLogin
 document.addEventListener("DOMContentLoaded", function(){
     
 })
-
-let dados = [
-    { email: 'guilherme@gmail', senha: '6767' },
-    { email: 'fulano@gmail', senha: '4321' },
-    { email: 'tal@gmail', senha: '1234' },
-];
--- controller/login/login_dados.js
+++ controller/login/login_dados.js
@@ -1,3 +1,11 @@
+import { puxarLogin } from "./login/login_google.js";
+
+window.puxarLogin = puxarLogin
+
+document.addEventListener("DOMContentLoaded", function(){
+    
+})
+
 let dados = [
     { email: 'guilherme@gmail', senha: '6767' },
     { email: 'fulano@gmail', senha: '4321' },
