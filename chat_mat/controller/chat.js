debd843cbfb7776948baea3459ae34b5cd281624
Murilo2602 <borgesmurilo2603@gmail.com>
2024-10-29 14:54:29 -0300

    yi,kl.uo

--- chat_mat/controller/chat.js
+++ chat_mat/controller/chat.js
@@ -1,34 +1,5 @@
-import { criarPost } from './../view/index.js';
+import { criarPost } from "./../view/index.js"
 
-document.addEventListener("DOMContentLoaded", function () {
-    criarPost();
-
-    const enviarBtn = document.getElementById('post_enviar');
-    const input = document.getElementById('digite');
-
-    // Verifica se os elementos existem
-    if (enviarBtn && input) {
-        enviarBtn.addEventListener('click', function () {
-            let postContent = input.value;
-
-            if (postContent.trim() !== '') {
-                let novoPost = {
-                    nome: 'Você',
-                    aroba: 'seu_usuario',
-                    post: postContent,
-                    tempo: 'agora',
-                    comentario: 'xxx',
-                    curtida: 'xxx',
-                };
-
-                // Adiciona o novo post ao array
-                posts.unshift(novoPost);
-                criarPost(); // Atualiza a renderização
-
-                input.value = ''; // Limpa o campo de entrada
-            }
-        });
-    } else {
-        console.error("Botão ou campo de entrada não encontrado.");
-    }
-});
+document.addEventListener("DOMContentLoaded", function (){
+    criarPost()
+})
