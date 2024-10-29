0f98bb9475ee2b0745f3ecdf16b70ac36729df4e
Murilo2602 <borgesmurilo2603@gmail.com>
2024-10-29 15:13:21 -0300

    ta certo esse

--- chat_mat/view/index.js
+++ chat_mat/view/index.js
@@ -1,7 +1,8 @@
-let posts = [
+let post = [
     {
         nome: 'Fulano',
         aroba: 'praticio_star',
+        // fotoP: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCFdPq2T2g20oo4EePsxh1Siabxqn-Tujjpw&s', // URL de exemplo
         post: 'Se mate Bryan',
         tempo: '25h',
         comentario: 'xxx',
@@ -10,6 +11,7 @@ let posts = [
     {
         nome: 'Fulano',
         aroba: 'praticio_star',
+        // fotoP: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCFdPq2T2g20oo4EePsxh1Siabxqn-Tujjpw&s', // URL de exemplo
         post: 'Se mate Bryan',
         tempo: '25h',
         comentario: 'xxx',
@@ -18,71 +20,77 @@ let posts = [
 ];
 
 export function criarPost() {
+
     let seccao = document.getElementById('bola');
-    seccao.innerHTML = ''; // Limpa a seção antes de renderizar
 
-    for (let i = 0; i < posts.length; i++) {
-        let postView = document.createElement('div');
-        postView.className = 'post_view';
+    for (let i = 0; i < post.length; i++) {
 
-        let postVc = document.createElement('div');
-        postVc.className = 'post_vc';
+        let postView = document.createElement('div')
+        postView.id = 'post_view'
 
-        let postVci = document.createElement('div');
-        postVci.className = 'post_vci';
+        let postVc = document.createElement('div')
+        postVc.id = 'post_vc'
 
-        let postImgV = document.createElement('img');
-        postImgV.className = 'post_img_v'; // Adicione uma classe para estilização
+        let postVci = document.createElement('div')
+        postVci.id = 'post_vci'
 
-        let postNome = document.createElement('h1');
-        postNome.className = 'post_Vnome';
-        postNome.textContent = posts[i].nome;
+        let postImgV = document.createElement('img')
+        postImgV.id = 'post_img_v'
 
-        let postArrouba = document.createElement('h1');
-        postArrouba.className = 'post_Varrouba';
-        postArrouba.textContent = posts[i].aroba;
+        let postNome = document.createElement('h1')
+        postNome.id = 'post_Vnome'
+        postNome.textContent = post[i].nome
 
-        let postTempo = document.createElement('h1');
-        postTempo.className = 'post_Vmin';
-        postTempo.textContent = posts[i].tempo;
+        let postArrouba = document.createElement('h1')
+        postArrouba.id = 'post_Varrouba'
+        postArrouba.textContent = post[i].aroba
 
-        let postText = document.createElement('div');
-        postText.className = 'post_text';
-        postText.textContent = posts[i].post;
+        let postTempo = document.createElement('h1')
+        postTempo.id = 'post_Vmin'
+        postTempo.textContent = post[i].tempo
 
-        let postBotoes = document.createElement('div');
-        postBotoes.className = 'post_botoes';
+        let postV = document.createElement('div')
+        postV.id = 'post'
 
-        let comentario = document.createElement('button');
-        comentario.className = 'comentar';
-        comentario.textContent = posts[i].comentario;
+        let postBotoes = document.createElement('div')
+        postBotoes.id = 'post_botoes'
 
-        let icoment = document.createElement('i');
-        icoment.className = 'fa-solid fa-comment';
+        let comentario = document.createElement('button')
+        comentario.id = 'comentar'
+        comentario.style.justifyContent = 'space-evenly'
+        comentario.textContent = post[i].comentario
 
-        let curtir = document.createElement('button');
-        curtir.className = 'curtir';
-        curtir.textContent = posts[i].curtida;
+        let icoment = document.createElement('i')
+        icoment.className = 'fa-solid fa-comment'
+        icoment.id = 'comentar_i'
 
-        let ilike = document.createElement('i');
-        ilike.className = 'fa-solid fa-heart';
+        let curtir = document.createElement('button')
+        curtir.id = 'curtir'
+        curtir.style.justifyContent = 'space-evenly'
+        curtir.textContent = post[i].curtida
+
+        let ilike = document.createElement('i')
+        ilike.className = 'fa-solid fa-heart'
+        ilike.id = 'curtir_i'
 
         // Montando a estrutura do post
         comentario.appendChild(icoment);
         curtir.appendChild(ilike);
-
         postBotoes.appendChild(comentario);
         postBotoes.appendChild(curtir);
 
         postVci.appendChild(postImgV);
+
         postVc.appendChild(postVci);
         postVc.appendChild(postNome);
         postVc.appendChild(postArrouba);
-        postVc.appendChild(postTempo);
+        postVc.appendChild(postTempo); // Corrigido aqui, de post_Vmin para postTempo
+
         postView.appendChild(postVc);
-        postView.appendChild(postText);
+        postView.appendChild(postV);
         postView.appendChild(postBotoes);
 
         seccao.appendChild(postView);
-    }
-}
+    };
+
+};
