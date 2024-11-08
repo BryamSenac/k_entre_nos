2a1513eac878141739733d069fafbd710f3d4df8
LF6809 <miguel.8986@aluno.pr.senac.br>
2024-11-08 16:08:43 -0300

    minha_tela_bola

--- controller/propaganda.js/banner.js
+++ controller/propaganda.js/banner.js
@@ -1,5 +1,25 @@
 let banners = document.getElementById('banners');
+let numeroAtual = 0;
 
 export function trocarBanner(numerobanner) {
+    numeroAtual = numerobanner;
     banners.style.left = '-' + numerobanner + '00vw'
+
+
+for (let i = 0; i < 3; i++) {
+    document.getElementById('btn' + i).classList.remove('botao-ativo');
+    document.getElementById('btn' + i).classList.add('botao');
+}
+document.getElementById('btn' + numerobanner).classList.add('botao-ativo');
+}
+
+export function iniciarTrocaAutomatica(){
+    setInterval(() => {
+        if (numeroAtual >=2) {
+            numeroAtual = 0;
+        } else {
+            numeroAtual += 1;
+        }
+        trocarBanner(numeroAtual);
+    }, 2500);
 }
--- view/js/perfil.js
+++ view/js/perfil.js
@@ -2,9 +2,11 @@ import { criaPerfil } from "./../../controller/perfil/operacao_K.js";
 import { esconderATLZ } from "./../../controller/perfil/edit.js";
 import { mostrarATLZ } from "./../../controller/perfil/edit.js";
 import { trocarBanner } from "../../controller/propaganda.js/banner.js";
+import { iniciarTrocaAutomatica } from "../../controller/propaganda.js/banner.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     criaPerfil();
+    iniciarTrocaAutomatica();
 });
 
 window.esconderATLZ = esconderATLZ;
