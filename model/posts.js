7f7d9671812b1863b02eae235545eb1299507867
Tapioka-788 <guilhermezuchellifelipetto@gmail.com>
2024-11-08 17:13:02 -0300

    add vetores

--- model/posts.js
+++ model/posts.js
@@ -1,5 +1,32 @@
 export let post = [
     {
+        nome: 'Bryan Mussolini',
+        aroba: 'ainholini',
+        fotoP: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCFdPq2T2g20oo4EePsxh1Siabxqn-Tujjpw&s', // URL de exemplo
+        msg: 'algue me enssina iscrev',
+        tempo: '30min',
+        comentario: '2',
+        curtida: '1k',  
+    },
+    {
+        nome: 'Juan',
+        aroba: 'juanzera',
+        fotoP: 'https://th.bing.com/th/id/OIP.QiITpBxI48YazeXpyv_dKAHaG9?rs=1&pid=ImgDetMain', // URL de exemplo
+        msg: 'Como fazer uma bomba atomica em casa? sem os outros descobrirem',
+        tempo: '2h',
+        comentario: '297',
+        curtida: '118',
+    },
+    {
+       nome: 'Bryan Mussolini',
+       aroba: 'ainholini',
+       fotoP: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCFdPq2T2g20oo4EePsxh1Siabxqn-Tujjpw&s', // URL de exemplo
+       msg: 'poqrue dues cirou o php?',
+       tempo: '4h',
+       comentario: '7',
+       curtida: '19k',  
+   },
+    {
         nome: 'Pedro Volta',
         aroba: 'praticio_star',
         fotoP: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCFdPq2T2g20oo4EePsxh1Siabxqn-Tujjpw&s', // URL de exemplo
@@ -9,29 +36,21 @@ export let post = [
         curtida: '59',
     },
     {
-        nome: 'Bryan Mussolini',
-        aroba: 'ainholini',
-        fotoP: 'https://th.bing.com/th/id/OIP.js561WkT1-ouiGm8lL5l2wHaE8?rs=1&pid=ImgDetMain', // URL de exemplo
-        msg: 'Qual o melhor programa de talentos infantis?',
-        tempo: '2h',
+        nome: 'Otravio Bittrencourt',
+        aroba: 'luciano',
+        fotoP: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCFdPq2T2g20oo4EePsxh1Siabxqn-Tujjpw&s', // URL de exemplo
+        msg: 'Qual a melhor receita de presunto com melao',
+        tempo: '222h',
         comentario: '297',
-        curtida: '118',
-    },
+        curtida: '118',  
+    },   
     {
-        nome: 'Otravio Bittrencourt',
-        aroba: 'luciano',
-        fotoP: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCFdPq2T2g20oo4EePsxh1Siabxqn-Tujjpw&s', // URL de exemplo
-        msg: 'Qual a melhor receita de presunto com melao',
-        tempo: '222h',
-        comentario: '297',
-        curtida: '118',  
-    },    {
-        nome: 'Otravio Bittrencourt',
-        aroba: 'luciano',
-        fotoP: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCFdPq2T2g20oo4EePsxh1Siabxqn-Tujjpw&s', // URL de exemplo
-        msg: 'Qual a melhor receita de presunto com melao',
-        tempo: '222h',
-        comentario: '297',
-        curtida: '118',  
-    }
+        nome: 'Ddd',
+        aroba: 'Pdy',
+        fotoP: 'https://th.bing.com/th/id/OIP.js561WkT1-ouiGm8lL5l2wHaE8?rs=1&pid=ImgDetMain', // URL de exemplo
+        msg: 'Onde assistir The voice kids',
+        tempo: '14anos',
+        comentario: '5364',
+        curtida: '200k',
+    },
 ];
